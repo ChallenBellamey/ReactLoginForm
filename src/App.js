bce851ef4841ec4542b216d95b7e877bea2286ed
challenbellamey <challenbellamey@hotmail.com>
2019-11-21 20:32:01 +0000

    Added language property to app state to dictate which language is displayed to user

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import LanguageInput from './components/LanguageInput.js';
 
-function App() {
-  return (
-    <div className="App">
-      <LanguageInput language='french'/>
+class App extends Component {
+  state = {
+    language: 'english'
+  }
+
+  render () {
+    const {language} = this.state;
+
+    return <div className="App">
+      <LanguageInput language={language}/>
     </div>
-  );
+  };
 }
 
 export default App;
