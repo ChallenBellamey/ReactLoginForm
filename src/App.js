0c36ed26ed3fc1ebf8d4cbfacd6ed91ce93ebc59
challenbellamey <challenbellamey@hotmail.com>
2019-11-21 20:42:52 +0000

    Created setLanguage method for app, allowing language to changed in state

--- src/App.js
+++ src/App.js
@@ -5,15 +5,21 @@ import LanguageInput from './components/LanguageInput.js';
 class App extends Component {
   state = {
     language: 'english'
-  }
+  };
 
   render () {
     const {language} = this.state;
 
     return <div className="App">
-      <LanguageInput language={language}/>
+      <LanguageInput language={language} setLanguage={this.setLanguage}/>
     </div>
   };
-}
+
+  setLanguage = (language) => {
+    this.setState({
+      language
+    });
+  };
+};
 
 export default App;
