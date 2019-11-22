3327dc3853ad067ed351ac198b3347feeeccbed2
challenbellamey <challenbellamey@hotmail.com>
2019-11-22 01:11:47 +0000

    Passed updatePassword method to PasswordInput component to allow user input to be conveyed to LoginMenu state

--- src/components/LoginMenu/subcomponents/PasswordInput.js
+++ src/components/LoginMenu/subcomponents/PasswordInput.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-export default function PasswordInput ({password_label}) {
+export default function PasswordInput ({password_label, password, updatePassword}) {
     return <div classname='password_input'>
         <p classname='password_label'>
             {password_label}
         </p>
-        <input type="password" classname='password_textbox'>
-
+        <input type="password" classname='password_textbox'
+                value={password}
+                onChange={(event) => updatePassword(event.nativeEvent.target.value)}>
         </input>
     </div>
 };
